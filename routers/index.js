e4119a387a7e06aa6e0909c1a8b2579fd1b5539a
Min Bhone Thant <95273099+Antaraes@users.noreply.github.com>
2023-10-23 11:28:35 +0630

    Update:node version and Feat:Get all users

--- routers/index.js
+++ routers/index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const route = express.Router();
 const userController = require("../controllers/userController");
+const userModel = require("../models/user");
 route.get("/", (req, res) => {
   res.send("Hi");
 });
 
 route.post("/users", userController.createUser);
+route.get("/users", async (req, res) => {
+  const users = await userModel.find();
+  res.json(users);
+});
 
 module.exports = route;
